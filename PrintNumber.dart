934a22886abc2725ac166146e3ed267519239874
Saksham Gupta <sakshamgupta391@gmail.com>
2022-01-16 02:03:31 +0530

    day 1: Hello World

---
+++ PrintNumber.dart
@@ -0,0 +1,11 @@
+// Define a function
+printNumber(num aNumber) {
+  print(
+      'The number is $aNumber'); // Print to the console, (I know ; is missing)
+}
+
+// This is where the app starts executing.
+void main() {
+  var number = 42; // Declaring and initializing a variable
+  printNumber(number); // Call a function
+}
